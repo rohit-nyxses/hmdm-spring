4b905139fd7f9fd23f78b579abd3aa3114b0e620
rohit-nyxses <rohit@nyxses.com>
2025-07-31 22:56:45 +0530

    Initial commit

---
+++ server/src/main/java/com/hmdm/HmdmServerApplication.java
@@ -0,0 +1,11 @@
+package com.hmdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmdmServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HmdmServerApplication.class, args);
+    }
+}
